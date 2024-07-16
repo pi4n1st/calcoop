2004748ec67c5bdb24cac3a4cd7f5aa551fef65c
sharkova <afelera@mail.ru>
2024-07-16 11:05:58 +0300

    first commit

---
+++ src/main/java/ru/krista/example/App.java
@@ -0,0 +1,15 @@
+package ru.krista.example;
+
+
+public final class App {
+    private App() {
+    }
+
+    
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.getEquation();
+        calc.doMath();
+        calc.printResult();
+}
+}
